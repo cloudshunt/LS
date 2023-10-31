9bded0a15111f6e6575018c4d775f02118bb473a
cloudshunt <cloudshunt@gmail.com>
2023-10-30 19:53:27 -0700

    added Asking the user for another calculation feature

--- js101/lesson_2/calculator.js
+++ js101/lesson_2/calculator.js
@@ -18,17 +18,6 @@ function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
-function inputNum() {
-  let num = readline.question();
-
-  while (invalidNumber(num)) {
-    prompt("not a valid number, try again");
-    num = readline.question();
-  }
-
-  return num;
-}
-
 // prompt("Welcome to Calculator!");
 
 // prompt("What's the first number?");
@@ -39,50 +28,113 @@ function inputNum() {
 //   number1 = readline.question();
 // }
 
-console.log('Second number?');
-let number2 = readline.question();
+// console.log('Second number?');
+// let number2 = readline.question();
 
-while (invalidNumber(number2)) {
-  prompt("not valid number, try again");
-  number2 = readline.question();
-}
+// while (invalidNumber(number2)) {
+//   prompt("not valid number, try again");
+//   number2 = readline.question();
+// }
 
-console.log(
-  // eslint-disable-next-line no-multi-str
-  "Select an operation.\n\
-1) Add 2) Subtract 3) Multiply 4) Divide"
-);
-let operation = readline.question();
+// console.log(
+//   // eslint-disable-next-line no-multi-str
+//   "Select an operation.\n\
+// 1) Add 2) Subtract 3) Multiply 4) Divide"
+// );
+// let operation = readline.question();
 
-while (!['1','2','3','4'].includes(operation)) {
-  prompt('Must choose 1,2,3 or 4');
-  operation = readline.question();
-}
+// while (!['1','2','3','4'].includes(operation)) {
+//   prompt('Must choose 1,2,3 or 4');
+//   operation = readline.question();
+// }
 
-let output;
-switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
-    break;
-  case '2':
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':
-    output = Number(number1) / Number(number2);
-    break;
-}
+// let output;
+// switch (operation) {
+//   case '1':
+//     output = Number(number1) + Number(number2);
+//     break;
+//   case '2':
+//     output = Number(number1) - Number(number2);
+//     break;
+//   case '3':
+//     output = Number(number1) * Number(number2);
+//     break;
+//   case '4':
+//     output = Number(number1) / Number(number2);
+//     break;
+// }
 
-console.log(`result is ${output}`);
+// console.log(`result is ${output}`);
 
-prompt('continue or not? 1 = yes, 2 = no ');
-let end = readline().question;
-while (end) {
+let continueProgram = true;
+
+while (continueProgram) {
   prompt("Welcome to Calculator!");
 
   prompt("What's the first number?");
-  let number1 = inputNum();
+  let number1 = readline.question();
+
+  while (invalidNumber(number1)) {
+    prompt("not a valid number, try again");
+    number1 = readline.question();
+  }
 
   console.log('Second number?');
+  let number2 = readline.question();
+
+  while (invalidNumber(number2)) {
+    prompt("not valid number, try again");
+    number2 = readline.question();
+  }
+
+  console.log(
+    // eslint-disable-next-line no-multi-str
+    "Select an operation.\n\
+  1) Add 2) Subtract 3) Multiply 4) Divide"
+  );
+  let operation = readline.question();
+
+  while (!['1','2','3','4'].includes(operation)) {
+    prompt('Must choose 1,2,3 or 4');
+    operation = readline.question();
+  }
+
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  console.log(`result is ${output}`);
+
+  // prompt('Do another round of calculation? 1 = yes, 2 = no ');
+  // end = readline.question();
+
+  // if (end === '1') {continue} //continue program
+  // else if (end === '2') {continueProgram = false}
+  // else {console.log('invalid input, try again.')}
+
+  while (true) {
+    prompt('Do another round of calculation? 1 = yes, 2 = no ');
+    let end = readline.question();
+
+    if (end === '1') {
+      continueProgram = true;
+      break;
+    } else if (end === '2') {
+      continueProgram = false;
+      break;
+    } else {console.log('invalid input, try again.') }
+  }
+}
+
