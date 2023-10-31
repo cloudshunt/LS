5c23dc3cd5baeef78c1fa2d2994fd0ada175e478
cloudshunt <cloudshunt@gmail.com>
2023-10-30 22:13:35 -0700

    modified calc 2

--- js101/lesson_2/calculator.js
+++ js101/lesson_2/calculator.js
@@ -1,4 +1,6 @@
 const readline = require('readline-sync');
+const msgJson = require('./calculator_messages.json');
+
 
 function prompt(message) {
   console.log(`=> ${message}`);
@@ -8,52 +10,65 @@ function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
-prompt("Welcome to Calculator!");
+let continueProgram = true;
 
-prompt("What's the first number?");
-let number1 = readline.question();
+while (continueProgram) {
+  prompt(msgJson.greetMessage); //json greet //done
 
-while (invalidNumber(number1)) {
-  prompt("not a valid number, try again");
-  number1 = readline.question();
+  prompt("What's the first number?");
+  let number1 = readline.question();
+
+  while (invalidNumber(number1)) {
+    prompt(msgJson.notValidNumMessage); //json notValidNumMessage DONE
+    number1 = readline.question();
+  }
+
+  console.log('Second number?');
+  let number2 = readline.question();
+
+  while (invalidNumber(number2)) {
+    prompt(msgJson.notValidNumMessage); //json notValidNum DONE
+    number2 = readline.question();
+  }
+
+  console.log( msgJson.selectOperationMessage); //json selectOperation DONE
+  let operation = readline.question();
+
+  while (!['1','2','3','4'].includes(operation)) {
+    prompt(msgJson.invalidOperationSelectMessage); //json invalidOperationSelectMessage DONE
+    operation = readline.question();
+  }
+
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  console.log(`result is ${output}`);
+
+
+  while (true) {
+    prompt(msgJson.repeatProgramSelectionMessage); //json repeatProgramSelection
+    let end = readline.question();
+
+    if (end === '1') {
+      continueProgram = true;
+      break;
+    } else if (end === '2') {
+      continueProgram = false;
+      break;
+    } else {console.log('invalid input, try again.') }
+  }
 }
 
-console.log('Second number?');
-let number2 = readline.question();
-
-while (invalidNumber(number2)) {
-  prompt("not valid number, try again");
-  number2 = readline.question();
-}
-
-console.log(
-  // eslint-disable-next-line no-multi-str
-  "Select an operation.\n\
-1) Add 2) Subtract 3) Multiply 4) Divide"
-);
-let operation = readline.question();
-
-while (!['1','2','3','4'].includes(operation)) {
-  prompt('Must choose 1,2,3 or 4');
-  operation = readline.question();
-}
-
-let output;
-switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
-    break;
-  case '2':
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':
-    output = Number(number1) / Number(number2);
-    break;
-}
-
-console.log(`result is ${output}`);
-
-console.log('hi');
