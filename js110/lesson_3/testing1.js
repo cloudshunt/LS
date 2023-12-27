4853dd7fe55ebd888ef49bce0d42242e6d4cad68
cloudshunt <cloudshunt@gmail.com>
2023-12-26 20:57:43 -0800

    twentyOne with added features

--- js110/lesson_3/testing1.js
+++ js110/lesson_3/testing1.js
@@ -1,33 +1,11 @@
-const INITIAL_MARKER = ' ';
-const WINNING_LINES = [
-  ['a1','a2','a3'], ['b1','b2','b3'], ['c1','c2','c3'], // rows
-  ['a1','b1','c1'], ['a2','b2','c2'], ['a3','b3','c3'], // columns
-  ['a1','b2','c3'], ['a3','b2','c1']             // diagonals
-];
+if (who === 'player') {
+  while (true) {
+    prompt('Enter 1 = hit, 2 = stay');
+    let input = READLINE.question();
 
-function findAtRiskSquare(board) {
-  let riskExist = false;
-  for (let idx = 0; idx < WINNING_LINES.length; idx += 1) {
-    console.log(WINNING_LINES[idx]);
-    riskExist = WINNING_LINES[idx].filter(square => board[square] === 'X') === 2;
-    if (riskExist) {
-      let potentialRiskSquare = Object.values(WINNING_LINES[idx]);
-      let riskSquare = potentialRiskSquare.filter(square => board[square] === 'X');
-      return riskSquare;
-    }
+    if (input === '1') hit(who, deck, handTracker);
+
+    displayHands(handTracker);
+
   }
 }
-
-function initializeBoard() {
-  let board = {
-    a1: INITIAL_MARKER, a2: INITIAL_MARKER, a3: INITIAL_MARKER,
-    b1: INITIAL_MARKER, b2: INITIAL_MARKER, b3: INITIAL_MARKER,
-    c1: INITIAL_MARKER, c2: INITIAL_MARKER, c3: INITIAL_MARKER,
-  };
-
-  return board;
-}
-
-let board = initializeBoard();
-
-findAtRiskSquare(board);
--- js110/lesson_3/twentyOne.js
+++ js110/lesson_3/twentyOne.js
@@ -1,61 +1,9 @@
-/**
- * initializeDeck Algo:
- * ------------------
- * ????
- * this logic is used when delt ace?
- * function aceOneOrEleven(points) {
- *  return points > 10 ? 1:11;
- * }
- * 
- * const SUITS = ['Hearts', 'Diamonds', 'Clubs', and 'Spades']
- * const VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10,
- *                Jack, Queen, King, Ace]
- * const POINTS = {
- *  2: 2, 3: 3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9,
- *  10:10, Jack: 10, Queen:10, King: 10,
- *  Ace: aceOneOrEleven(points) OR Null?
- * }
- * SET deckArr = []
- * SET id = 0
- *
- * LOOP SUITS:
- *  LOOP VALUES:
- *    initializeCard(id, cardSuit, cardValue) append to deckArr
- *    id += 1
- * 
- * InitializeCard(id, cardSuit, cardValue) Algo:
- *  SET cardObj = {};
- *  cardObj[STR(id)] = {
- *    suit: cardSuit
- *    value: cardValue
- *    points: POINTS[value]
- * }
- * ----------------
- * data structure for players
- */
-
-// use this when delt cards?
-function aceOneOrEleven(points) {
-  return points > 10 ? 1 : 11;
-}
-
+/* eslint-disable max-lines-per-function */
+const READLINE = require('readline-sync');
 const SUITS = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
 const VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
-const POINTS = {
-  2:2,
-  3:3,
-  4:4,
-  5:5,
-  6:6,
-  7:7,
-  8:8,
-  9:9,
-  10:10,
-  Jack: 10,
-  Queen: 10,
-  King: 10,
-  Ace: null
-};
+const WINNING_POINTS = 21;
+const DEALER_AIM_POINTS = 17;
 
 function shuffle(deck) {
   for (let index = deck.length - 1; index > 0; index--) {
@@ -66,13 +14,10 @@ function shuffle(deck) {
 
 function initializeDeck() {
   let deckArr = [];
-  let id = 0;
 
   SUITS.forEach((cardSuit) => {
     VALUES.forEach((cardValue => {
-      //console.log(id, cardSuit, cardValue);
-      deckArr.push(initializeCard(id, cardSuit, cardValue));
-      id += 1;
+      deckArr.push(initializeCard( cardSuit, cardValue));
     }));
   });
 
@@ -81,77 +26,277 @@ function initializeDeck() {
   return deckArr;
 }
 
-function initializeCard(id, cardSuit, cardValue) {
+function initializeCard(cardSuit, cardValue) {
   let cardObj = {};
-  //                    id : {misc info}
-  // example output: { '50': { suit: 'Spades', value: 'King', points: 10 } }
-  cardObj[String(id)] = {
-    suit: cardSuit,
-    value: cardValue,
-    points: POINTS[cardValue]
-  };
+
+  cardObj['suit'] = cardSuit;
+  cardObj['value'] = cardValue;
 
   return cardObj;
 }
 
 function initializeHand() {
-  let handTracker = {
-    player:{hand:[], points:0},
-    dealer:{hand:[], points:0}
-  };
+  let handTracker = { player:{hand:[], total: 0}, dealer:{hand:[], total: 0} };
   return handTracker;
 }
 
-/**
- * card is either delt to player or dealer, every deal gives one card
- */
+function calcTotal(who, handTracker) {
+  let values = handTracker[who].hand.map((card) => {
+    return card.value;
+  });
 
+  let sum = 0;
+  values.forEach(value => {
+    if (value === 'Ace') sum += 11;
+    else if (['Jack','Queen','King'].includes(value)) sum += 10;
+    else sum += value;
+  });
 
+  // Aces adjustment
+  values
+    .filter((value) => value === 'Ace')
+    .forEach(() => {
+      if (sum > WINNING_POINTS) sum -= 10;
+    });
 
-function updateHand(who, cardToAdd, handTracker) {
-  handTracker[who]['hand'].push(cardToAdd);
-
-  let cardID = Object.keys(cardToAdd).join(); //to get card id as stand alone string
-  handTracker[who]['points'] += cardToAdd[cardID].points;
-
-  return handTracker;
-
-}
-
-function dealCard(who, deck, handTracker) {
-  let card = deck.shift();
-  return updateHand(who, card, handTracker);
+  handTracker[who].total = sum;
+  return sum;
 }
 
 function initialDeal(deck, handTracker) {
-  handTracker = dealCard("player", deck, handTracker);
-  handTracker = dealCard("dealer", deck, handTracker);
+  dealCard("player", deck, handTracker);
+  dealCard("dealer", deck, handTracker);
 
-  handTracker = dealCard("player", deck, handTracker);
-  handTracker = dealCard("dealer", deck, handTracker);
-
-  return handTracker;
+  dealCard("player", deck, handTracker);
+  dealCard("dealer", deck, handTracker);
 }
 
 function prompt(message) {
   console.log(`=>${message}`);
 }
-function displayHands(handTracker) {
-  console.log(handTracker.dealer);
-  //let dealerValue = handTracker.dealer;
-  //prompt(`Dealer has: ${dealerValue} and unknown card`); //unknown card will be idx 0
-  prompt(`You have:`);
+
+// eslint-disable-next-line consistent-return
+function joinAnd(arr, delimeter = ', ', lastDelimeter = 'and') {
+  lastDelimeter = " " + lastDelimeter + " ";
+  if (arr.length === 0) return "";
+  else if (arr.length === 1) return arr.join();
+  else if (arr.length === 2) return arr.join(lastDelimeter);
+  else if (arr.length >= 3) {
+    let allNumStrExceptLast = arr.slice(0, arr.length - 1).join(delimeter);
+    let lastStrNum = arr.slice(arr.length - 1);
+    let result = allNumStrExceptLast + lastDelimeter + lastStrNum;
+    return result;
+  }
 }
+
+// eslint-disable-next-line consistent-return
+function handDisplayLoop(someonesHand, who, endCondition = false) {
+  let someonesHandValueArr = someonesHand.map((card) => {
+    return card.value;
+  });
+
+  // Omit the 0th idx for display, as it stays unkown to the player till
+  // dealer's turn occurs
+  if (who === 'dealer') {
+    let unknownCard = someonesHandValueArr.shift();
+    if (endCondition === false) {
+      someonesHandValueArr.push('unknown card');
+      return joinAnd(someonesHandValueArr);
+    } else if (endCondition === true) {
+      let unknownCardMessage = `the unknown card is ${unknownCard}!!!`;
+      someonesHandValueArr.push(unknownCardMessage);
+      return joinAnd(someonesHandValueArr);
+    }
+  }
+
+  if (who === 'player') return joinAnd(someonesHandValueArr);
+
+}
+
+function displayHands(handTracker, endCondition) {
+  let yourSum = handTracker['player'].total;//calcTotal('player', handTracker);
+  let dealerHand = handTracker.dealer.hand;
+  prompt(`Dealer has: ${handDisplayLoop(dealerHand, "dealer", endCondition)}`);//unknown card will be idx 0
+
+  let yourHand = handTracker.player.hand;
+  prompt(`You have: ${handDisplayLoop(yourHand, "player", endCondition)} (${yourSum})\n`);
+}
+
+function bustOrNot(sum) {
+  if (sum <= WINNING_POINTS) return false;
+  else return true;
+}
+
+function dealCard(who, deck, handTracker) {
+  let card = deck.shift();
+  handTracker[who]['hand'].push(card);
+
+  let sum = calcTotal(who,handTracker);
+  handTracker[who].total = sum;
+  //return sum;
+}
+
+function dealingLogic(who, deck, handTracker, endCondition = false) {
+  dealCard(who, deck, handTracker);
+  let sum = handTracker[who].total;
+  let bust = bustOrNot(sum);
+  endCondition = bust;
+  displayHands(handTracker, endCondition);
+
+  return bust;
+}
+
+function playerPrompt(who, deck, handTracker) {
+  let bust = false;
+
+  while (true) {
+    prompt('Enter 1 = hit, 2 = stay');
+    let input = READLINE.question();
+
+    if (input === '1') {
+      console.clear();
+      prompt(`You Hit`);
+      bust = dealingLogic(who, deck, handTracker);
+    } else if (input === '2') {
+      console.clear();
+      prompt(`You Hold`);
+      break;
+    } else {
+      prompt('Invalid entry, try again!');
+    }
+
+    if (bust) break;
+  }
+
+  return bust;
+}
+
+function dealerLogic(who, deck, handTracker) {
+  let bust = false;
+  let dealerTurn = true;
+  let sum = handTracker[who].total;
+
+  while (true) {
+    if (sum < DEALER_AIM_POINTS) {
+      prompt(`Dealer hits`);
+      bust = dealingLogic(who, deck, handTracker, dealerTurn);
+      sum = handTracker[who].total;
+      if (bust) break;
+
+    } else if (sum >= DEALER_AIM_POINTS) {
+      prompt(`Dealer holds`);
+      break;
+    }
+  }
+
+  return bust;
+}
+
+
+function announceResult(result, handTracker, grandScore) {
+  let playerSum = handTracker['player'].total;
+  let dealerSum = handTracker['dealer'].total;
+
+  switch (result) {
+    case "you busted":
+      grandScore.dealer += 1;
+      prompt(`You busted at ${playerSum}, Dealer wins at ${dealerSum}`);
+      break;
+    case "dealer busted":
+      grandScore.player += 1;
+      prompt(`Dealer busted at ${dealerSum}, You win at ${playerSum}!`);
+      break;
+    case "player":
+      grandScore.player += 1;
+      prompt(`You Win! your score: ${playerSum}, dealer score: ${dealerSum}`);
+      break;
+    case "dealer":
+      grandScore.dealer += 1;
+      prompt(`Dealer wins! your score: ${playerSum}, dealer score: ${dealerSum}`);
+      break;
+    case "tie":
+      prompt(`It's a tie! The score is ${playerSum}`);
+  }
+}
+
+function findResult(handTracker) {
+  let playerSum = handTracker['player'].total;
+  let dealerSum = handTracker['dealer'].total;
+
+  if (playerSum > WINNING_POINTS) return "you busted";
+  else if (dealerSum > WINNING_POINTS) return "dealer busted";
+  else if (playerSum > dealerSum) return "player";
+  else if (dealerSum > playerSum) return "dealer";
+  else return "tie";
+
+}
+
+// eslint-disable-next-line max-statements
+function round(deck, handTracker, grandScore) {
+  let result;
+  let playerBust = playerPrompt("player", deck, handTracker);
+  if (playerBust) {
+    result = findResult(handTracker);
+    announceResult(result, handTracker, grandScore);
+    return;
+  }
+
+  let dealerBust = dealerLogic("dealer", deck, handTracker);
+  if (dealerBust) {
+    result = findResult(handTracker);
+    announceResult(result, handTracker, grandScore);
+    return;
+  }
+
+  let endCondition = true;
+  displayHands(handTracker, endCondition);
+  findResult(handTracker);
+  result = findResult(handTracker);
+  announceResult(result, handTracker, grandScore);
+}
+
+
+// eslint-disable-next-line max-statements
 function startGame() {
-  let deck = initializeDeck();
-  let handTracker = initializeHand();
-  handTracker = initialDeal(deck, handTracker);
+  let grandScore = {player:0, dealer:0};
+  let continueGame = true;
 
-  displayHands(handTracker);
+  while (continueGame) {
+    let deck = initializeDeck();
+    let handTracker = initializeHand();
 
+    initialDeal(deck, handTracker);
+    displayHands(handTracker);
 
-  //console.log();
+    round(deck, handTracker, grandScore);
+
+    if (grandScore.player === 3) {
+      prompt(`You are the Grand Winner, woah!!! Goodbye.`);
+      break;
+    } else if (grandScore.dealer === 3) {
+      prompt(`Dealer is the Grand Winner, yikes~ GoodBye`);
+      break;
+    }
+
+    while (true) {
+      prompt(`Another round? 1 = yes 2 = no`);
+      let input = READLINE.question();
+      if (input === '1') {
+        console.clear();
+        break;
+      } else if (input === '2') {
+        prompt('Thank you for playing, goodbye!');
+        continueGame = false;
+        break;
+      } else prompt(`invalid input, try again`);
+    }
+  }
+
 
 }
 
-startGame();
+function runProgram() {
+  startGame();
+}
+
+runProgram();
