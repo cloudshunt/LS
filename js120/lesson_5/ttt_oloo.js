7ffb897c5e773810ddac3534c245e302b81b0847
cloudshunt <cloudshunt@gmail.com>
2024-03-05 13:33:32 -0800

    lesson 5 mod

--- js120/lesson_5/ttt_oloo.js
+++ js120/lesson_5/ttt_oloo.js
@@ -1,49 +1,42 @@
 let readline = require('readline-sync');
 
-class Square {
-  static UNUSED_SQUARE = " ";
-  static HUMAN_MARKER = "X";
-  static COMPUTER_MARKER = "O";
+let Square = {
+  UNUSED_SQUARE:   " ",
+  HUMAN_MARKER:    "X",
+  COMPUTER_MARKER: "O",
 
-  constructor(marker = Square.UNUSED_SQUARE) {
-    // We need some way to keep track of this square's marker.
+  init(marker = Square.UNUSED_SQUARE) {
     this.marker = marker;
-  }
-
-  getMarker() {
-    return this.marker;
-  }
-
-  isUnused() {
-    return this.marker === Square.UNUSED_SQUARE;
-  }
-
-  setMarker(marker) {
-    this.marker = marker;
-  }
+    return this;
+  },
 
   toString() {
     return this.marker;
-  }
-}
+  },
 
-class Board {
-  constructor() {
-    // We need a way to model the 3x3 grid. Perhaps "squares"?
-    // What data structure should we use? An Array? An Object? Something else?
-    // What should the data structure store? Strings? Numbers? Square objects?
+  setMarker(marker) {
+    this.marker = marker;
+  },
+
+  isUnused() {
+    return this.marker === Square.UNUSED_SQUARE;
+  },
+
+  getMarker() {
+    return this.marker;
+  },
+};
+
+
+let Board = {
+  init() {
     this.squares = {};
     for (let counter = 1; counter <= 9; ++counter) {
-      this.squares[String(counter)] = new Square();
+      this.squares[String(counter)] = Object.create(Square).init();
     }
-  }
 
-  displayWithClear() {
-    console.clear();
-    console.log("");
-    console.log("");
-    this.display();
-  }
+    return this;
+  },
 
   countMarkersFor(player, keys) {
     let markers = keys.filter(key => {
@@ -51,16 +44,16 @@ class Board {
     });
 
     return markers.length;
-  }
+  },
 
   unusedSquares() {
     let keys = Object.keys(this.squares);
     return keys.filter(key => this.squares[key].isUnused());
-  }
+  },
 
   markSquareAt(key, marker) {
     this.squares[key].setMarker(marker);
-  }
+  },
 
   display() {
     console.log("");
@@ -76,42 +69,41 @@ class Board {
     console.log(`  ${this.squares["7"]}  |  ${this.squares["8"]}  |  ${this.squares["9"]}`);
     console.log("     |     |");
     console.log("");
-  }
-}
+  },
 
-class Player {
-  constructor(marker) {
-    // maybe a "marker" to keep track of this player's symbol (i.e., 'X' or 'O')
+  displayWithClear() {
+    console.clear();
+    console.log("");
+    console.log("");
+    this.display();
+  },
+};
+
+let Player = {
+  init(marker = Square.UNUSED_SQUARE) {
     this.marker = marker;
-  }
+    return this;
+  },
 
   getMarker() {
     return this.marker;
   }
-}
+};
 
-class Human extends Player {
-  constructor() {
-    super(Square.HUMAN_MARKER);
+let Human = {
+  init() {
+    return Object.create(Player).init(Square.HUMAN_MARKER);
   }
-}
+};
 
-class Computer extends Player {
-  constructor() {
-    super(Square.COMPUTER_MARKER);
+let Computer = {
+  init() {
+    return Object.create(Player).init(Square.COMPUTER_MARKER);
   }
-}
+};
 
-
-class TTTGame {
-  constructor() {
-    // Need a board and two players
-    this.board = new Board();
-    this.human = new Human();
-    this.computer = new Computer();
-  }
-
-  static POSSIBLE_WINNING_ROWS = [
+let TTTGame = {
+  POSSIBLE_WINNING_ROWS : [
     [ "1", "2", "3" ],            // top row of board
     [ "4", "5", "6" ],            // center row of board
     [ "7", "8", "9" ],            // bottom row of board
@@ -120,32 +112,39 @@ class TTTGame {
     [ "3", "6", "9" ],            // right column of board
     [ "1", "5", "9" ],            // diagonal: top-left to bottom-right
     [ "3", "5", "7" ],            // diagonal: bottom-left to top-right
-  ];
+  ],
+
+  init() {
+    this.board = Object.create(Board).init();
+    this.human = Object.create(Human).init();
+    this.computer = Object.create(Computer).init();
+    return this;
+  },
 
   gameOver() {
     return this.boardIsFull() || this.someoneWon();
-  }
+  },
 
   boardIsFull() {
     let unusedSquares = this.board.unusedSquares();
     return unusedSquares.length === 0;
-  }
+  },
 
   someoneWon() {
     return this.isWinner(this.human) || this.isWinner(this.computer);
-  }
+  },
 
   displayWelcomeMessage() {
     //STUB
     console.clear();
     console.log("Welcome to Tic Tac Toe!");
     console.log("");
-  }
+  },
 
   displayGoodbyeMessage() {
     //STUB
     console.log("Thanks for playing Tic Tac Toe! Goodbye!");
-  }
+  },
 
   displayResults() {
     if (this.isWinner(this.human)) {
@@ -155,14 +154,13 @@ class TTTGame {
     } else {
       console.log("A tie game. How boring.");
     }
-  }
+  },
 
   isWinner(player) {
     return TTTGame.POSSIBLE_WINNING_ROWS.some(row => {
       return this.board.countMarkersFor(player, row) === 3;
     });
-  }
-
+  },
 
   humanMoves() { // was firstPlayerMoves
     let choice;
@@ -180,7 +178,7 @@ class TTTGame {
 
     // mark the selected square with the human's marker
     this.board.markSquareAt(choice, this.human.getMarker());
-  }
+  },
 
   computerMoves() { // was secondPlayerMoves
     let validChoices = this.board.unusedSquares();
@@ -191,7 +189,7 @@ class TTTGame {
     } while (!validChoices.includes(choice));
 
     this.board.markSquareAt(choice, this.computer.getMarker());
-  }
+  },
 
 
   play() {
@@ -213,8 +211,7 @@ class TTTGame {
     this.displayResults();
     this.displayGoodbyeMessage();
   }
-}
+};
 
-
-let game = new TTTGame();
+let game = Object.create(TTTGame).init();
 game.play();
